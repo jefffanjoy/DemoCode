1650958137bbefd554b49886d4fcbb5d4be571ad
jefffanjoy <jfanjoy@hotmail.com>
2017-09-05 17:45:51 -0300

    .

---
+++ Runbooks/Azure Storage/Remove-BlobFromAzureStorage.ps1
@@ -0,0 +1,21 @@
+﻿Param (
+    [Parameter(Mandatory=$true)]
+    [string] $StorageAccountName,
+    [Parameter(Mandatory=$true)]
+    [string] $StorageAccountKey,
+    [Parameter(Mandatory=$true)]
+    [string] $ContainerName,
+    [Parameter(Mandatory=$true)]
+    [string] $BlobName
+)
+
+$context = New-AzureStorageContext `
+    -StorageAccountName $StorageAccountName `
+    -StorageAccountKey $StorageAccountKey
+
+$result = Get-AzureStorageBlobContent `
+    -Blob $BlobName `
+    -Container $ContainerName `
+    -Context $context
+
+$result
