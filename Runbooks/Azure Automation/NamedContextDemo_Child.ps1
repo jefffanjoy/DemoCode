523041c87c06df6e223246aaeef468a8b4b89d08
jefffanjoy <jfanjoy@hotmail.com>
2018-04-05 03:03:35 -0300

    Initial commit.

---
+++ Runbooks/Azure Automation/NamedContextDemo_Child.ps1
@@ -0,0 +1,13 @@
+﻿$SubscriptionId = '140aa888-06f1-45b7-9457-17d5aa7c1b33'
+
+Write-Output "Logging into Azure..."
+$ServicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
+Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $ServicePrincipalConnection.TenantId `
+    -ApplicationId $ServicePrincipalConnection.ApplicationId `
+    -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint
+
+$AzureContext = Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+Get-AzureRmContext -DefaultProfile $AzureContext | Format-List
---
+++ Runbooks/Azure Automation/NamedContextDemo_Parent.ps1
@@ -0,0 +1,23 @@
+﻿Write-Output "Logging into Azure..."
+$ServicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
+Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $ServicePrincipalConnection.TenantId `
+    -ApplicationId $ServicePrincipalConnection.ApplicationId `
+    -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint
+
+$AzureContext = Select-AzureRmSubscription -SubscriptionId $ServicePrincipalConnection.SubscriptionID
+
+$ChildRunbookName = 'NamedContextDemo_Child'
+$Instances = 100
+$AutomationAccountName = 'Testing4'
+$ResourceGroupName = 'Testing4'
+
+1..$Instances | ForEach-Object {
+    Write-Output ("Starting instance {0} of child runbook {1}." -f $_, $ChildRunbookName)
+    Start-AzureRmAutomationRunbook `
+        -ResourceGroupName $ResourceGroupName `
+        -AutomationAccountName $AutomationAccountName `
+        -Name $ChildRunbookName `
+        -DefaultProfile $AzureContext
+}
