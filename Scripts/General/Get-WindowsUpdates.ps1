095d2b25fb4c298708c3674ef615a32ed259c2d4
jefffanjoy <jfanjoy@hotmail.com>
2018-06-18 01:53:57 -0300

    Updated to be more useful for interactive troubleshooting.

--- Scripts/General/Get-WindowsUpdates.ps1
+++ Scripts/General/Get-WindowsUpdates.ps1
@@ -1,35 +1,36 @@
 # Get updates from Windows Updates
-$WUSession = New-Object -com "Microsoft.Update.Session";
-$WUSession.ClientApplicationID = "Install-Updates.ps1";
-$WUSearch = $WUSession.CreateUpdateSearcher();
-$SearchResults = $WUSearch.Search("IsHidden = 0 AND DeploymentAction=* AND IsInstalled =0");
-              
-$wuUpdates = @();
+$WUSession = New-Object -com "Microsoft.Update.Session"
+$WUSession.ClientApplicationID = "Get-WindowsUpdates.ps1"
+$WUSearch = $WUSession.CreateUpdateSearcher()
+$SearchResults = $WUSearch.Search("IsHidden = 0 AND DeploymentAction=* AND IsInstalled =0")
+
+$wuUpdates = @()
 foreach ($wuUpdate in $SearchResults.Updates) {
-    $update = @{
-        RevisionNumber       = $wuUpdate.Identity.RevisionNumber;
-        UpdateID             = $wuUpdate.Identity.UpdateID;
-        DeploymentAction     = $wuUpdate.DeploymentAction;
-        Title                = $wuUpdate.Title;
-        Deadline             = $wuUpdate.Deadline;
-        Description          = $wuUpdate.Description;
-        IsHidden             = $wuUpdate.IsHidden;
-        IsInstalled          = $wuUpdate.IsInstalled;
-        IsMandatory          = $wuUpdate.IsMandatory;
-        MsrcSeverity         = $wuUpdate.MsrcSeverity;
-        Type                 = $wuUpdate.Type;
-        RebootRequired       = $wuUpdate.RebootRequired;
-        IsPresent            = $wuUpdate.IsPresent;
-        AutoSelectOnWebSites = $wuUpdate.AutoSelectOnWebSites;
-        BrowseOnly           = $wuUpdate.BrowseOnly;
-        SecurityBulletinIDs  = @();
-    };
-              
+    $update = New-Object PSObject -Property @{
+        RevisionNumber       = $wuUpdate.Identity.RevisionNumber
+        UpdateID             = $wuUpdate.Identity.UpdateID
+        DeploymentAction     = $wuUpdate.DeploymentAction
+        Title                = $wuUpdate.Title
+        Deadline             = $wuUpdate.Deadline
+        Description          = $wuUpdate.Description
+        IsHidden             = $wuUpdate.IsHidden
+        IsInstalled          = $wuUpdate.IsInstalled
+        IsMandatory          = $wuUpdate.IsMandatory
+        MsrcSeverity         = $wuUpdate.MsrcSeverity
+        Type                 = $wuUpdate.Type
+        RebootRequired       = $wuUpdate.RebootRequired
+        IsPresent            = $wuUpdate.IsPresent
+        AutoSelectOnWebSites = $wuUpdate.AutoSelectOnWebSites
+        BrowseOnly           = $wuUpdate.BrowseOnly
+        SecurityBulletinIDs  = @()
+    }
+
     $wuUpdate.SecurityBulletinIDs | foreach {
         $update.SecurityBulletinIDs += $_
     }
-              
-    $wuUpdates += $update;
+
+    $wuUpdates += $update
 }
-              
-$wuUpdates | ConvertTo-Json | Out-File -FilePath wuUpdates.json; 
+
+#$wuUpdates | ConvertTo-Json | Out-File -FilePath wuUpdates.json 
+$wuUpdates | Format-Table UpdateID, RevisionNumber, Type, IsMandatory, Title
